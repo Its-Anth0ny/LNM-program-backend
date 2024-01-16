ca53a5dafca4b95eea9f542c542a0bd685a4a300
Its-Anth0ny <bhattshubhankar27@gmail.com>
2024-01-16 16:33:49 +0530

    db update

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,15 @@ const { Sequelize } = require('sequelize');
 require("dotenv").config();
 
 
-const sequelize = new Sequelize('herodb', 'anthony', 'anthony', {
-    host: 'localhost',
-    dialect: 'postgres'
+const sequelize = new Sequelize('herodashdb', 'anthony', 'XWC0iAqAQ6b1SgZ475Q4cLlePEaw8GKf', {
+    host: 'dpg-cmj34c8l5elc73epem2g-a.singapore-postgres.render.com',
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: false,
+          rejectUnauthorized: false,
+        },
+      },
 });
 
 module.exports = sequelize;
