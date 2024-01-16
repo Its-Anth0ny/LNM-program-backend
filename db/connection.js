8e6da60a9252e1d85f2e28c66d2632b2d9def88f
Its-Anth0ny <bhattshubhankar27@gmail.com>
2024-01-16 16:06:57 +0530

    final commit

--- db/connection.js
+++ db/connection.js
@@ -6,16 +6,5 @@ const sequelize = new Sequelize('herodb', 'anthony', 'anthony', {
     host: 'localhost',
     dialect: 'postgres'
 });
-// const sequelize = new Sequelize(process.env.DB_URL, {
-//   dialect: "postgres",
-//   logging: false,
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
 
-
-// });
 module.exports = sequelize;
